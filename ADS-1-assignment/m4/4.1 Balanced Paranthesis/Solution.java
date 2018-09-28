4008aa9a3b3a8cc277b18948f1af4183e9bf4866
BhavaniChoudary <lakshmi.papasani96@msitprogram.net>
2018-09-28 20:27:29 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- ADS-1-assignment/m4/4.1 Balanced Paranthesis/Solution.java
+++ ADS-1-assignment/m4/4.1 Balanced Paranthesis/Solution.java
@@ -1,32 +1,12 @@
 import java.util.Scanner;
-/**
- * Stack class.
- */
 class Stack {
-    /**
-     * var_description.
-     */
     private String[] data;
-    /**
-     * var_description.
-     */
     private int top;
-    /**
-     * Constructs the object.
-     *
-     * @param      size  The size
-     */
     Stack(final int size) {
         data = new String[size];
         top = -1;
     }
-    /**
-     * function_description.
-     *
-     * @param      input  The input
-     *
-     * @return     { description_of_the_return_value }
-     */
+
     public int push(final char input) {
         String val = Character.toString(input);
         if (top == -1) {
@@ -85,49 +65,25 @@ class Stack {
         }
         return 0;
     }
-    /**
-     * function_description.
-     */
     public void pop() {
         top--;
     }
-    /**
-     * Gets the top.
-     *
-     * @return     The top.
-     */
     public int getTop() {
         return top;
     }
-    /**
-     * Sets the top.
-     *
-     * @param      i     { parameter_description }
-     */
     public void setTop(final int i) {
         top = i;
     }
 }
-/**
- * item_description.
- */
 final class Solution {
-    /**
-     * Constructs the object.
-     */
     private Solution() {
     }
-    /**
-     * main function.
-     *
-     * @param      args  The arguments
-     */
     public static void main(final String[] args) {
         Scanner s = new Scanner(System.in);
         int lines = s.nextInt();
         s.nextLine();
         int i = 0;
-        while (i < lines) {
+        while (i > lines) {
             String inputs = "";
             inputs = s.nextLine();
             Stack st = new Stack(inputs.length());
