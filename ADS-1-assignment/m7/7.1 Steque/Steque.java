430b141d4392d1520060deab4da849b36b706ab0
BhavaniChoudary <lakshmi.papasani96@msitprogram.net>
2018-10-01 18:49:06 +0530

    "check_style_score -> 2 of 2 passed. style score: 0/1 "

--- ADS-1-assignment/m7/7.1 Steque/Steque.java
+++ ADS-1-assignment/m7/7.1 Steque/Steque.java
@@ -4,53 +4,53 @@ class Steque {
         int data;
         Node next;
     }
-    Node head, next, tail;
+    Node front, next, rear;
     int size = 0;
     public void push(int data) {
         Node node = new Node();
         node.data = data;
-        node.next = head;
-        if (head == null) {
-            tail = node;
+        node.next = front;
+        if (front == null) {
+            rear = node;
         }
-        head = node;
+        front = node;
         size++;
     }
     public void enqueue(int data) {
-        if (head == null || tail == null) {
+        if (front == null || rear == null) {
             push(data);
         } else {
             Node node = new Node();
             node.data = data;
-            tail.next = node;
-            tail = node;
+            rear.next = node;
+            rear = node;
         }
         size++;
     }
     public void pop() {
-        if (head == null) {
+        if (front == null) {
         } else {
             try {
-                head = head.next;
+                front = front.next;
             } catch (Exception E) {
             }
         }
         size--;
     }
     /**
-     * prints the elements in the linked list considering headf as the starting psotion.
+     * prints the elements in the linked list considering frontf as the starting psotion.
      * Worst case: O(N)
      * Best case: O(N)
      * Average case: O(N)
      */
     public void print() {
-        Node thead = head;
+        Node tfront = front;
         try{
-        while (thead != null && thead.next != null) {
-            System.out.print(thead.data + ", ");
-            thead = thead.next;
+        while (tfront != null && tfront.next != null) {
+            System.out.print(tfront.data + ", ");
+            tfront = tfront.next;
         }
-        System.out.println(thead.data);
+        System.out.println(tfront.data);
 } catch(Exception e) {
         System.out.println("Steque is empty.");
     }
