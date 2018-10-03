748db370ad49994c5d37922a64c0ab8d554c1484
BhavaniChoudary <lakshmi.papasani96@msitprogram.net>
2018-10-04 01:46:21 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- ADS-1-assignment/m9/9.1 Merge Sort Improvements/Solution.java
+++ ADS-1-assignment/m9/9.1 Merge Sort Improvements/Solution.java
@@ -1,8 +1,19 @@
 import java.util.Scanner;
+/**.
+ *This is a class Solution.
+ */
  final class Solution {
+    /**.
+     * It's a default constructor.
+     */
     private Solution() {
 
     }
+    /**.
+     * Main Method
+     *
+     * @param      args  The arguments
+     */
     public static void main(final String[] args) {
         Mergesort merge = new Mergesort();
         Scanner sc = new Scanner(System.in);
