c2ee3d547741f0e6177c9b5f932f9689bf034d11
BhavaniChoudary <lakshmi.papasani96@msitprogram.net>
2018-10-12 23:13:32 +0530

    "taxi_cab_numbers: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-assignment/m17/TaxiCabNumbers/Solution.java
+++ ADS-1-assignment/m17/TaxiCabNumbers/Solution.java
@@ -1,22 +1,68 @@
 import java.util.Scanner;
+/**
+ * Class for taxi number.
+ */
 class Taxinum implements Comparable<Taxinum> {
+	/**
+	 * integer number1 var_description.
+	 */
 	private int number1;
+	/**
+	 * integer number2 var_description.
+	 */
 	private int number2;
+	/**
+	 * integer sum var_description.
+	 */
 	private int sum;
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      number1  The number 1
+	 * @param      number2  The number 2
+	 */
 	Taxinum(final int number1, final int number2) {
         this.sum = number1 * number1 * number1 + number2 * number2 * number2;
         this.number1 = number1;
         this.number2 = number2;
     }
+    /**
+     * get method for num1.
+     * Time complexity is 1.
+     *
+     * @return     { description_of_the_return_value }
+     */
     public int getnumber1() {
         return this.number1;
     }
+    /**
+     * get method for num2.
+     * Time complexity is 1.
+     *
+     * @return     { description_of_the_return_value }
+     */
+
     public int getnumber2() {
         return this.number2;
     }
+    /**
+     * get sum.
+     * Time complexity is 1.
+     *
+     * @return     { description_of_the_return_value }
+     */
+
     public int getsum() {
         return this.sum;
     }
+    /**
+     * compares.
+     * Time complexity is 1.
+     *
+     * @param      t     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
     public int compareTo(final Taxinum t) {
         if (this.sum < t.sum) {
             return -1;
@@ -25,14 +71,32 @@ class Taxinum implements Comparable<Taxinum> {
         }
         return 0;
     }
+    /**
+     * Returns a string representation of the object.
+     * Time complexity is 1.
+     *
+     * @return     String representation of the object.
+     */
     public String toString() {
         return sum + " = " + number1 + "^3" + " + " + number2 + "^3";
     }
 }
+/**
+ * Solution class.
+ */
 public final class Solution {
+	/**
+	 * Constructs the object.
+	 * Time complexity is 1.
+	 */
 	private Solution() {
 		//constructor not used.
 	}
+	/**
+	 * Main function.
+	 *
+	 * @param      args  The arguments
+	 */
 	public static void main(String[] args) {
 		final int number = 600;
 		Scanner s = new Scanner(System.in);
