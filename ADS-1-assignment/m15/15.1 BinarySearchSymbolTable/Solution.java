c1b56f41e0fec097587e61cfe7909e12e8a31b1f
BhavaniChoudary <lakshmi.papasani96@msitprogram.net>
2018-10-12 15:11:55 +0530

    "ads1_m15_a1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-assignment/m15/15.1 BinarySearchSymbolTable/Solution.java
+++ ADS-1-assignment/m15/15.1 BinarySearchSymbolTable/Solution.java
@@ -10,13 +10,30 @@ class SymbolTable<Key extends Comparable<Key>, Value> {
 	 * array for keys.
 	 */
 	private Key[] keys;
+	/**
+	 * array for values.
+	 */
 	private Value[] values;
+	/**
+	 * var size description.
+	 */
 	private int size;
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      n     { parameter_description }
+	 */
 	SymbolTable(final int n) {
 		keys = (Key[]) new Comparable[n];
 		values = (Value[]) new Object[n];
 		size = 0;
 	}
+	/**
+	 * inserts key and a value in the arrays. 
+	 *
+	 * @param      key    The key
+	 * @param      value  The value
+	 */
 	void put(final Key key, final Value value) {
 		if (value == null) {
 			delete(key);
@@ -35,9 +52,23 @@ class SymbolTable<Key extends Comparable<Key>, Value> {
         values[i] = value;
         size++;
 	}
+	/**
+	 * shows if key is in symboltable.
+	 *
+	 * @param      key   The key
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
 	boolean contains(final Key key) {
         return get(key) != null;
     }
+    /**
+     * return value paired with Key.
+     *
+     * @param      key   The key
+     *
+     * @return     { description_of_the_return_value }
+     */
     Value get(final Key key) {
         if (size == 0) {
             return null;
@@ -48,6 +79,13 @@ class SymbolTable<Key extends Comparable<Key>, Value> {
         }
         return null;
     }
+    /**
+     * returns no.of keys less than key.
+     *
+     * @param      key   The key
+     *
+     * @return     { description_of_the_return_value }
+     */
     int rank(final Key key) {
         int low = 0, high = size - 1;
         while (low <= high) {
@@ -63,9 +101,19 @@ class SymbolTable<Key extends Comparable<Key>, Value> {
         }
         return low;
     }
+    /**
+     * gives largest key.
+     *
+     * @return     { description_of_the_return_value }
+     */
     Key max() {
         return keys[size - 1];
     }
+    /**
+     * deletes.
+     *
+     * @param      key   The key
+     */
     void delete(final Key key) {
         if (size == 0) {
             return;
@@ -83,9 +131,15 @@ class SymbolTable<Key extends Comparable<Key>, Value> {
         keys[j] = null;
         values[j] = null;
     }
+    /**
+     * deletes smallest key.
+     */
     void deleteMin() {
         delete(keys[0]);
     }
+    /**
+     * returns all keys in sorted order.
+     */
     void keys() {
         for (int i = 0; i < size; i++) {
             if (values[i] != null) {
@@ -93,6 +147,13 @@ class SymbolTable<Key extends Comparable<Key>, Value> {
             }
         }
     }
+    /**
+     * return largest key less than or equal to key.
+     *
+     * @param      key   The key
+     *
+     * @return     { description_of_the_return_value }
+     */
     Key floor(final Key key) {
         int i = rank(key);
         if (i < size && key.compareTo(keys[i]) == 0) {
@@ -104,9 +165,20 @@ class SymbolTable<Key extends Comparable<Key>, Value> {
         return keys[i - 1];
     }
 }
+/**
+ * Solution class.
+ */
 final class Solution {
+	/**
+	 * Constructs the object.
+	 */
 	private Solution() {
     }
+    /**
+     * Main function_description.
+     *
+     * @param      args  The arguments
+     */
     public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
         String[] input = sc.nextLine().split(" ");
