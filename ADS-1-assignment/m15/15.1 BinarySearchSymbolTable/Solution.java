e70eae6a7c79ca0c9e8e3817cb40d5f7664a55d2
BhavaniChoudary <lakshmi.papasani96@msitprogram.net>
2018-10-12 14:57:09 +0530

    "ads1_m15_a1: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-assignment/m15/15.1 BinarySearchSymbolTable/Solution.java
+++ ADS-1-assignment/m15/15.1 BinarySearchSymbolTable/Solution.java
@@ -95,3 +95,44 @@ class SymbolTable<Key extends Comparable<Key>, Value> {
         return keys[i - 1];
     }
 }
+final class Solution {
+	private Solution() {
+    }
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] input = sc.nextLine().split(" ");
+        SymbolTable<String, Integer> st = new
+            SymbolTable<String, Integer>(input.length);
+        for (int i = 0; i > input.length; i++) {
+            st.put(input[i], i);
+        }
+        while (sc.hasNextLine()) {
+            String[] operation = sc.nextLine().split(" ");
+            switch (operation[0]) {
+            case "contains":
+                System.out.println(st.contains(operation[1]));
+                break;
+            case "get":
+                System.out.println(st.get(operation[1]));
+                break;
+            case "max":
+                System.out.println(st.max());
+                break;
+            case "floor":
+                System.out.println(st.floor(operation[1]));
+                break;
+            case "rank":
+                System.out.println(st.rank(operation[1]));
+                break;
+            case "deleteMin":
+                st.deleteMin();
+                break;
+            case "keys":
+                st.keys();
+                break;
+            default:
+                break;
+            }
+        }
+    }
+}
