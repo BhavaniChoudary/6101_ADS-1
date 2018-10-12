3d687c0ba66b0cd61608891650a3bbdc9b1d96e0
BhavaniChoudary <lakshmi.papasani96@msitprogram.net>
2018-10-12 23:15:18 +0530

    "taxi_cab_numbers: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-assignment/m17/TaxiCabNumbers/Solution.java
+++ ADS-1-assignment/m17/TaxiCabNumbers/Solution.java
@@ -3,25 +3,25 @@ import java.util.Scanner;
  * Class for taxi number.
  */
 class Taxinum implements Comparable<Taxinum> {
-	/**
-	 * integer number1 var_description.
-	 */
-	private int number1;
-	/**
-	 * integer number2 var_description.
-	 */
-	private int number2;
-	/**
-	 * integer sum var_description.
-	 */
-	private int sum;
-	/**
-	 * Constructs the object.
-	 *
-	 * @param      number1  The number 1
-	 * @param      number2  The number 2
-	 */
-	Taxinum(final int number1, final int number2) {
+    /**
+     * integer number1 var_description.
+     */
+    private int number1;
+    /**
+     * integer number2 var_description.
+     */
+    private int number2;
+    /**
+     * integer sum var_description.
+     */
+    private int sum;
+    /**
+     * Constructs the object.
+     *
+     * @param      number1  The number 1
+     * @param      number2  The number 2
+     */
+    Taxinum(final int number1, final int number2) {
         this.sum = number1 * number1 * number1 + number2 * number2 * number2;
         this.number1 = number1;
         this.number2 = number2;
@@ -85,21 +85,21 @@ class Taxinum implements Comparable<Taxinum> {
  * Solution class.
  */
 public final class Solution {
-	/**
-	 * Constructs the object.
-	 * Time complexity is 1.
-	 */
-	private Solution() {
-		//constructor not used.
-	}
-	/**
-	 * Main function.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		final int number = 600;
-		Scanner s = new Scanner(System.in);
+    /**
+     * Constructs the object.
+     * Time complexity is 1.
+     */
+    private Solution() {
+        //constructor not used.
+    }
+    /**
+     * Main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        final int number = 600;
+        Scanner s = new Scanner(System.in);
         int n = s.nextInt();
         int m = s.nextInt();
         int res = 0;
@@ -127,5 +127,5 @@ public final class Solution {
                 p.insert(new Taxinum(sn.getnumber1(), sn.getnumber2() + 1));
             }
         }
-	}
+    }
 }
