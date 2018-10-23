f52bc67718243c3926310f023fffa2b5b5e9a558
BhavaniChoudary <lakshmi.papasani96@msitprogram.net>
2018-10-23 23:43:19 +0530

    "ads1_m19_a1: 0/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-assignment/m19/19.1 BST/Solution.java
+++ ADS-1-assignment/m19/19.1 BST/Solution.java
@@ -1,6 +1,19 @@
 import java.util.Scanner;
+/**
+ * Solution class.
+ */
 final class Solution {
-	private Solution() {}
+	/**
+	 * Constructs the object.
+	 */
+	private Solution() {
+		//function.
+	}
+	/**
+	 * Main function_description.
+	 *
+	 * @param      args  The arguments
+	 */
     public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
         BinaryST bst = new BinaryST();
