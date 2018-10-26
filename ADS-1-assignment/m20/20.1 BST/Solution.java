3a03fbec59580de1f980c1b9c7b767562e62732e
BhavaniChoudary <lakshmi.papasani96@msitprogram.net>
2018-10-26 19:56:50 +0530

    ": 2/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-assignment/m20/20.1 BST/Solution.java
+++ ADS-1-assignment/m20/20.1 BST/Solution.java
@@ -14,13 +14,14 @@ final class Solution {
      *
      * @param      args  The arguments
      */
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         Scanner sc =  new Scanner(System.in);
         BinaryST bst = new BinaryST();
-        while(sc.hasNextLine()) {
+        while (sc.hasNextLine()) {
             String[] tokens = sc.nextLine().split(",");
-            switch(tokens[0]) {
-                case "put": bst.put(new Book(tokens[1], tokens[2], Double.parseDouble(tokens[2+1])), Integer.parseInt(tokens[2+2]));
+            switch (tokens[0]) {
+                case "put": bst.put(new Book(tokens[1], tokens[2],
+                    Double.parseDouble(tokens[2 + 1])), Integer.parseInt(tokens[2 + 2]));
                 break;
                 case "get":
                 System.out.println(bst.get(new Book(tokens[1],
